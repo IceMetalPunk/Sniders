abd7f2102d3ef196b34ae2a2ac2cdf5fd5509a6d
Kevin <IceMetalPunk@gmail.com>
2015-03-18 20:49:48 -0400

    Created table restore files. Stu must run the scripts to restore TAR and TAB. See A_Version_File.txt for information.

--- RestoreTables.php
+++ RestoreTables.php
@@ -65,18 +65,37 @@
 
 	$tabCreate = "CREATE TABLE IF NOT EXISTS `t-a-billing` (
 	`TAB-CUSTNO` varchar(6) NOT NULL COMMENT 'Customer Number',
-	 `TAB-POST-DT` date NOT NULL COMMENT 'Date Posted', `TAB-INV-DT` date NOT NULL COMMENT 'Date Invoiced (If An Invoice)',
+	 `TAB-POST-DT` date NOT NULL COMMENT 'Date Posted',
+	 `TAB-INV-DT` date NOT NULL COMMENT 'Date Invoiced (If An Invoice)',
 	 `TAB-INV-NO` varchar(8) NOT NULL COMMENT 'Invoice Number (If An Invoice)',
-	 `TAB-ADJ-NO` varchar(8) NOT NULL DEFAULT '' COMMENT 'Adjustnment Number (Adjustments Only)',
+	 `TAB-ADJ-NO` varchar(8) NOT NULL DEFAULT '' COMMENT 'Adjustment Number (Adjustments Only)',
 	 `TAB-ADJ-TYPE` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Adjustment Type (0=Bill; 1=Invoice; See Adjust.php For Type Codes)',
 	 `TAB-ADJ-REF` varchar(29) NOT NULL COMMENT 'Freeform Adjustment Info',
-	 `TAB-AMT` float NOT NULL DEFAULT '0' COMMENT 'Subtotal of non-adjustment charges', `TAB-DISCOUNT` float NOT NULL DEFAULT '0' COMMENT 'Discount Percentage (Optional?)', `TAB-TOTAL` float NOT NULL DEFAULT '0' COMMENT 'Amount After Discount Is Applied',
+	 `TAB-AMT` float NOT NULL DEFAULT '0' COMMENT 'Subtotal of non-adjustment charges',
+	 `TAB-DISCOUNT` float NOT NULL DEFAULT '0' COMMENT 'Discount Percentage (Optional?)',
+	 `TAB-TOTAL` float NOT NULL DEFAULT '0' COMMENT 'Amount After Discount Is Applied',
 	 PRIMARY KEY (`TAB-CUSTNO`,`TAB-INV-NO`,`TAB-ADJ-NO`),
 	 KEY `TAB-CUSTNO` (`TAB-CUSTNO`), KEY `TAB-CUSTNO_2` (`TAB-CUSTNO`)
 	) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores unbilled invoices and adjustments during a billing cycle';";
 	$executeTabCreate = mysql_query($tabCreate);
 
-	echo "<br />Blank TAB Creation Status: ".($executeTabCreate?"Successful":"Failed - ".mysql_error());
+	echo "<br />TAB Creation Status: ".($executeTabCreate?"Successful":"Failed - ".mysql_error());
+	
+	$tabInsert = "INSERT INTO `t-a-billing` (`TAB-CUSTNO`, `TAB-POST-DT`, `TAB-INV-DT`, `TAB-INV-NO`, `TAB-ADJ-NO`, `TAB-ADJ-TYPE`, `TAB-ADJ-REF`, `TAB-AMT`, `TAB-DISCOUNT`, `TAB-TOTAL`) VALUES
+		('11249', '', '2015-03-16', 'I15-0127', '', 1, '', 80, 0, 80),
+		('11514', '', '2015-03-16', 'I15-0128', '', 1, '', 944, 10, 849.60),
+		('11505', '', '2015-03-13', 'I15-0119', '', 1, '', 58, 0, 58),
+		('11277', '', '2015-03-13', 'I15-0121', '', 1, '', 50, 0, 50),
+		('11277', '', '2015-03-18', 'I15-0131', '', 1, '', 50, 0, 50),
+		('11195', '', '2015-03-16', 'I15-0126', '', 1, '', 50, 10, 45),
+		('11825', '', '2015-03-16', 'I15-0125', '', 1, '', 73, 10, 65.70),
+		('11133', '', '2015-03-13', 'I15-0123', '', 1, '', 85, 0, 85),
+		('23617', '', '2015-03-18', 'I15-0129', '', 1, '', 45, 0, 45),
+		('11250', '', '2015-03-16', 'I15-0120', '', 1, '', 132, 0, 132),
+		('10104', '', '2015-03-14', 'I15-0122', '', 1, '', 15, 0, 15),
+		('11833', '', '2015-03-14', 'I15-0130', '', 1, '', 20, 0, 20)";
+	$executeTabInsert = mysql_query($tabInsert);
+	echo "<br />TAB Insertion Status: ".($executeTabInsert?"Successful":"Failed - ".mysql_error());
 
 	/*
 	$invoiceView = "CREATE OR REPLACE VIEW `v-a-invoice` AS SELECT * FROM `t-work` where ((`W-INV-NO` = '0000000' OR `W-INV-NO`='') AND ((to_days(`W-USE-DT`) - to_days((select `l-DESC` from `t-lookup` where (`t-lookup`.`l-VALUE` = 301)))) <= 0))";
