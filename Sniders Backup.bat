3094fd500351faec876623c7b745e24cd28da9bd
Kevin <IceMetalPunk@gmail.com>
2015-03-18 23:01:03 -0400

    Added auto-backup batch file.

---
+++ Sniders Backup.bat
@@ -0,0 +1,14 @@
+@echo off
+mkdir DB_Backups
+
+echo Backing up database...
+"C:\wamp\bin\mysql\mysql5.6.12\bin\mysqldump.exe" -uroot -ptux898 sniders2013 > ./DB_Backups/snidersbackup.sql
+
+echo Backing up work ticket images...
+echo D | xcopy "C:\wamp\www\tickets" "DB_Backups/tickets" /E
+
+echo Backing up invoices and reports...
+echo D | xcopy "C:\wamp\www\billing" "DB_Backups/billing" /E
+
+echo Backup completed. Press ENTER.
+pause
