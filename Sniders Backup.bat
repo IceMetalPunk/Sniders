11071032f381d7c29ffd8bfaa40cc26e4da65755
Kevin <IceMetalPunk@gmail.com>
2015-03-18 23:17:29 -0400

    Quick update to database auto-backup script to prevent delay while overwriting existing backups.

--- Sniders Backup.bat
+++ Sniders Backup.bat
@@ -5,10 +5,10 @@ echo Backing up database...
 "C:\wamp\bin\mysql\mysql5.6.12\bin\mysqldump.exe" -uroot -ptux898 sniders2013 > ./DB_Backups/snidersbackup.sql
 
 echo Backing up work ticket images...
-echo D | xcopy "C:\wamp\www\tickets" "DB_Backups/tickets" /E
+echo D | xcopy "C:\wamp\www\tickets" "DB_Backups/tickets" /E /Y
 
 echo Backing up invoices and reports...
-echo D | xcopy "C:\wamp\www\billing" "DB_Backups/billing" /E
+echo D | xcopy "C:\wamp\www\billing" "DB_Backups/billing" /E /Y
 
 echo Backup completed. Press ENTER.
 pause
