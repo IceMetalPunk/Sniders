2a65302a616fc522decfee65e07990682a53f1b0
Kevin <IceMetalPunk@gmail.com>
2015-05-02 11:29:58 -0400

    Preventative bug-fixing with data backup scripts to exclude code from backups.

--- Sniders Backup.bat
+++ Sniders Backup.bat
@@ -4,11 +4,20 @@ mkdir DB_Backups
 echo Backing up database...
 "C:\wamp\bin\mysql\mysql5.6.12\bin\mysqldump.exe" -uroot -ptux898 sniders2013 > ./DB_Backups/snidersbackup.sql
 
-echo Backing up work ticket images...
-echo D | xcopy "C:\wamp\www\tickets" "DB_Backups/tickets" /E /Y
+echo .php>>ignore.txt
+echo .exe>>ignore.txt
+echo .css>>ignore.txt
 
 echo Backing up invoices and reports...
-echo D | xcopy "C:\wamp\www\billing" "DB_Backups/billing" /E /Y
+echo D | xcopy "C:\wamp\www\billing" "DB_Backups/billing" /E /Ys /exclude:ignore.txt
+
+
+echo .html>>ignore.txt
+
+echo Backing up work ticket images...
+echo D | xcopy "C:\wamp\www\tickets" "DB_Backups/tickets" /E /Y /exclude:ignore.txt
+
+rm ignore.txt
 
 echo Backup completed. Press ENTER.
 echo.
