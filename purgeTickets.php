36e628be24ac350e780818e4e57e856fe15b7aa4
Kevin <IceMetalPunk@gmail.com>
2015-09-04 17:18:29 -0400

    Alpha 3 of ticket purge.

--- purgeTickets.php
+++ purgeTickets.php
@@ -1,18 +1,18 @@
 <?php
 	$ticketDir="./tickets/Complete";
+	$purgeDir="./purged/Tickets";
+	if (!file_exists("./purged")) { mkdir("./purged"); }
+	if (!file_exists("./purges/Tickets")) { mkdir("./purged/Tickets"); }
+	
 	$files=scandir($ticketDir);
 	$oldTickets=array();
 	foreach ($files as $ind=>$filename) {
 		if ($filename=="." || $filename=="..") { continue; }
 		$ftime=filectime($ticketDir."/".$filename);
 		if ($ftime<mktime(0, 0, 0, 8, 1, 2015)) {
-			echo $filename." is older than 8/1/2015.<br />";
-			$oldTickets[]=$filename;
-		}
-		else {
-			echo $filename." is newer than 8/1/2015.<br />";
+			//echo $filename." is older than 8/1/2015.<br />";
+			rename($ticketDir."/".$filename, $purgeDir."/".$filename);
 		}
 	}
-	print_r($oldTickets);
 
 ?>
